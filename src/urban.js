3604d516c4b40f5333cae73d50fdbfb65e76629f
Herb <cpt.tenneal@gmail.com>
2016-08-06 14:49:35 -0500

    Added ~urban command to search Urban Dictionary for a user-provided phrase.

---
+++ src/urban.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const Promise = require('promise');
+const agent = require('superagent-promise')(require('superagent'), Promise);
+
+const urbanApi = "http://api.urbandictionary.com/v0/define?term=";
+
+let urban = {
+    "urban" : {
+        usage: "Searches Urban Dictionary for a user-provided search term",
+        delete: true,
+        type: "query",
+        cooldown: 5,
+        "process": (bot, msg, suffix) => {
+            agent('GET', `${urbanApi}${suffix}`)
+                .then(json => {
+                    let channel = msg.channel;
+
+                    let result = JSON.parse(json.res.text).list[0];
+
+                    let response = `**${suffix}**\n\n\`\`\`\n${result.definition}\n\`\`\`\n\n**Example:** ${result.example}\n<${result.permalink}>`;
+                    bot.sendMessage(channel, response);
+                });
+        }
+    }
+};
+
+exports.urban = urban;
