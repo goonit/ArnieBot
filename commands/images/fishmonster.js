0458ecf2a9324a0b3073638aef30441b907486d5
Herb <cpt.tenneal@gmail.com>
2018-01-16 13:25:59 -0600

    Fix Images not sending
    Needed to update images to be sent through the messageOptions.files object/property to properly send images to channels.

--- commands/images/fishmonster.js
+++ commands/images/fishmonster.js
@@ -2,19 +2,19 @@ const { Command } = require('discord.js-commando');
 const CONSTANTS = require('../../constants.js');
 
 module.exports = class FishMonster extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'fishmonster',
-      group: 'images',
-      memberName: 'fishmonster',
-      description: 'Posts a fishmonster image to the channel'
-    });
-  }
+	constructor(client) {
+		super(client, {
+			name: 'fishmonster',
+			group: 'images',
+			memberName: 'fishmonster',
+			description: 'Posts a fishmonster image to the channel'
+		});
+	}
 
-  async run(msg) {
-    let channel = msg.channel;
-    return msg
-      .delete()
-      .then(message => channel.send('', CONSTANTS.FISHMONSTER));
-  }
+	async run(msg) {
+		let channel = msg.channel;
+		return msg
+			.delete()
+			.then(message => channel.send('', { files: [CONSTANTS.FISHMONSTER] }));
+	}
 };
